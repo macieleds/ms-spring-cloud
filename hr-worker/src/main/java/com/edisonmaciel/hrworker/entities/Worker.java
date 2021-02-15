a2a1e3786f925ba1d526f78c198259dcb59909d7
emaciel <emaciel@uolinc.com>
2021-02-15 00:54:27 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/edisonmaciel/hrworker/entities/Worker.java
@@ -0,0 +1,31 @@
+package com.edisonmaciel.hrworker.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+}
