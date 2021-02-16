80c048faf32676f80faf39f3469de78378b10df3
emaciel <emaciel@uolinc.com>
2021-02-16 18:05:08 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/edisonmaciel/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/edisonmaciel/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.edisonmaciel.hrpayroll.resources;
 
 import com.edisonmaciel.hrpayroll.entities.Payment;
 import com.edisonmaciel.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,18 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment (@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
+    }
+
+    @GetMapping
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Bran", 400.0, days);
+        return ResponseEntity.ok(payment);
+
     }
 
 }
--- hr-worker/src/main/java/com/edisonmaciel/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/edisonmaciel/hrworker/resources/WorkerResource.java
@@ -27,13 +27,19 @@ public class WorkerResource {
     private WorkerRepository workerRepository;
 
     @GetMapping
-    public ResponseEntity<List<Worker>> findAll(){
+    public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = workerRepository.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Worker> findById(@PathVariable Long id){
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+//        try {
+//            Thread.sleep(3000L);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
