099960ec687d576f7d4711a148d8fe350ae135ef
emaciel <emaciel@uolinc.com>
2021-02-16 16:24:56 -0300

    Configuring Eureka clients

--- hr-payroll/src/main/java/com/edisonmaciel/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/edisonmaciel/hrpayroll/entities/Worker.java
@@ -1,16 +1,7 @@
 package com.edisonmaciel.hrpayroll.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.io.Serializable;
 
 @AllArgsConstructor
